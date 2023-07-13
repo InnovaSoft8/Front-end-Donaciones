87c24989d06adeec7bb657a92c0ca86d919669e8
Javiervas <javier_2193@live.com>
2023-07-13 13:23:02 -0500

    Servicio para comentario

---
+++ src/app/services/comentario.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Comentario } from '../models/comentario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComentarioService {
+  private apiUrl = 'URL_DEL_API'; // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) {}
+
+  // Obtener todos los comentarios
+  getComentarios(): Observable<Comentario[]> {
+    return this.http.get<Comentario[]>(`${this.apiUrl}/comentarios`);
+  }
+
+  // Obtener un comentario por su ID
+  getComentario(id: number): Observable<Comentario> {
+    return this.http.get<Comentario>(`${this.apiUrl}/comentarios/${id}`);
+  }
+
+  // Registrar un nuevo comentario
+  registrarComentario(comentario: Comentario): Observable<Comentario> {
+    return this.http.post<Comentario>(`${this.apiUrl}/comentarios`, comentario);
+  }
+
+  // Actualizar un comentario existente
+  actualizarComentario(comentario: Comentario): Observable<Comentario> {
+    return this.http.put<Comentario>(
+      `${this.apiUrl}/comentarios/${comentario.idComentario}`,
+      comentario
+    );
+  }
+
+  // Eliminar un comentario por su ID
+  eliminarComentario(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/comentarios/${id}`);
+  }
+}
