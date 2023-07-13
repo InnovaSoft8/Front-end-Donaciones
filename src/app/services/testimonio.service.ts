521467f0ebb36086eb1e39b717f750d1803ca40d
Javiervas <javier_2193@live.com>
2023-07-13 13:56:21 -0500

    Agregar servicio Testimonio

---
+++ src/app/services/testimonio.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Testimonio } from '../models/testimonio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestimonioService {
+  private apiUrl = 'URL_DEL_API'; // Reemplaza con la URL de tu API
+
+  constructor(private http: HttpClient) {}
+
+  // Obtener todos los testimonios
+  getTestimonios(): Observable<Testimonio[]> {
+    return this.http.get<Testimonio[]>(`${this.apiUrl}/testimonios`);
+  }
+
+  // Obtener un testimonio por su ID
+  getTestimonio(id: number): Observable<Testimonio> {
+    return this.http.get<Testimonio>(`${this.apiUrl}/testimonios/${id}`);
+  }
+
+  // Registrar un nuevo testimonio
+  registrarTestimonio(testimonio: Testimonio): Observable<Testimonio> {
+    return this.http.post<Testimonio>(`${this.apiUrl}/testimonios`, testimonio);
+  }
+
+  // Eliminar un testimonio por su ID
+  eliminarTestimonio(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/testimonios/${id}`);
+  }
+}
